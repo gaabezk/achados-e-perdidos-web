b0497b5b46e0e5dbb5de0fadbc3886f529854ee0
Gabriel Fernandes <gaabezk@gmail.com>
2023-04-13 14:16:00 -0300

    estrutura basica

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import {Cadastro} from './pages/Cadastro'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { Home } from './pages/Home/Index';
+import { ErrorPage } from './pages/Error';
 
+const routers = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/home",
+    element: <Cadastro />
+  }
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routers} />
   </React.StrictMode>,
 )
---
+++ src/pages/Cadastro/Index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Cadastro = () => {
+  return (
+    <div>
+      <h1>Cadastro</h1>
+    </div>
+  ) 
+};
